009b891e61ecbf2557202eae41a74b6df5063a8c
Kenneth NG <kwcng@hku.hk>
2019-12-11 02:20:46 +0000

    Add Query to get current user
    using the context to get the user id from the jwt-token then find the user by the id, it can be null so {nullable:true}

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -16,6 +16,7 @@ import { createRefreshToken, createAccessToken } from "./auth";
 import { isAuth } from "./isAuth";
 import { sendRefreshToken } from "./sendRefreshToken";
 import { getConnection } from "typeorm";
+import { verify } from "jsonwebtoken";
 
 @ObjectType()
 class LoginResponse {
@@ -40,6 +41,24 @@ export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Query(() => User, { nullable: true })
+  me(@Ctx() context: MyContext) {
+    const authorization = context.req.headers["authorization"];
+
+    if (!authorization) {
+      return null;
+    }
+
+    try {
+      const token = authorization.split(" ")[1];
+      const payload: any = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+      return User.findOne(payload.userId);
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   }
 
   @Mutation(() => LoginResponse)
