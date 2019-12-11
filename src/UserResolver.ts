68f0e539a8b097efbbc1b6bd7b64384fb0c61843
Kenneth NG <kwcng@hku.hk>
2019-12-11 02:09:21 +0000

    server first commit

---
+++ src/UserResolver.ts
@@ -0,0 +1,99 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+  Ctx,
+  UseMiddleware,
+  Int
+} from "type-graphql";
+import { User } from "./entity/User";
+import { hash, compare } from "bcryptjs";
+import { MyContext } from "./MyContext";
+import { createRefreshToken, createAccessToken } from "./auth";
+import { isAuth } from "./isAuth";
+import { sendRefreshToken } from "./sendRefreshToken";
+import { getConnection } from "typeorm";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "hi!!!";
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error("could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Wrong password");
+    }
+
+    // login success
+    //give token
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return {
+      accessToken: createAccessToken(user)
+    };
+  }
+
+  // do not use it in public , use it for internal use
+  // for testing only!!!
+  @Mutation(() => Boolean)
+  async revokeRefreshTokenForUser(@Arg("userId", () => Int) userId: number) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, "tokenVersion", 1);
+
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+}
